040e25fc42cd2b0d115883f5ad3e2dce2cc6338e
Your Name <you@example.com>
2017-08-23 14:11:58 +0800

    css and addId

--- pages/cal/cal.js
+++ pages/cal/cal.js
@@ -5,9 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    screenData:'0',
+    lastIsOpenator:false,
+    id1:'back',
+    id2:'clear',
+    id3:'negative',
+    id4:'+',
+    id5:'9',
+    id6:'8',
+    id7:'7',
+    id8:'-',
+    id9:'6',
+    id10:'5',
+    id11:'4',
+    id12:'*',
+    id13:'3',
+    id14:'2',
+    id15:'1',
+    id16:'/',
+    id17:'0',
+    id18:'.',
+    id19:'history',
+    id20:'='
   },
-
+  clickButton:function(e){
+    console.log(e.target.id)
+    var sd = this.data.screenData
+    var data
+    if(sd==0){
+      data = e.target.id
+    }else{
+      data = sd+e.target.id
+    }
+    this.setData({ screenData:data })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
